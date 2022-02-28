c7632baf048da5fc4fd6323a055068b365b25aac
Keve Lajsz <kevelajsz@icloud.com>
2022-02-28 15:00:11 +0100

    Chapter 12 drill (a g++ megengedte végre)

--- src/drillek/chapter12/main.cpp
+++ src/drillek/chapter12/main.cpp
@@ -2,9 +2,36 @@
 
 #include <iostream>
 
-int main(int argc, char** argv) {
-  Simple_window window(Graph_lib::Point(100, 100), 600, 400, "My Window");
-  window.wait_for_button();
+using namespace Graph_lib;
 
+int main(int argc, char** argv) {
+  Simple_window window(Point(100, 100), 600, 400, "My Window");
+
+  Axis xa(Axis::x, Point(20, 350), 280, 10, "x axis");
+  Axis ya(Axis::y, Point(20, 350), 280, 10, "y axis");
+  window.attach(xa); window.attach(ya);
+
+  Rectangle r(Point(200, 200), 100, 50);
+  r.set_fill_color(Color::green);
+  window.attach(r);
+
+  Text t(Point(200, 150), "Hello Graphics");
+  t.set_font_size(24);
+  t.set_font(Graph_lib::Font::helvetica_bold);
+  window.attach(t);
+
+  // Ez volt hirtelen a letöltések mappában
+  std::string imgp = "/home/klevcsoo/GitHub/deik-prog1/src/drillek/chapter12/raspi_logo.jpg";
+  Image img(Point(100, 100), imgp);
+  window.attach(img);
+
+  Circle c(Point(300, 300), 100);
+  window.attach(c);
+
+  Ellipse e(Point(550, 350), 100, 50);
+  e.set_fill_color(Color::dark_blue);
+  window.attach(e);
+
+  window.wait_for_button();
   return 0;
 }
